cb02585bd0aff53acd0be36cd7b2be3c5a4f0986
Zafar Akhtari <zafarakhtari@live.com>
2021-02-16 13:53:55 -0800

    commit

--- js/ex3.js
+++ js/ex3.js
@@ -1,12 +1,32 @@
-function sum_of_array(p_values){
-    let sum = 0;
-    for(let i=0; i < p_values.length ; i++){
-        sum += p_values[i];
-    }
-    return sum;
-}
 const values = [3, 11, 7, 2, 9, 10];
-console.log(`The array is: ${values}`);
-console.log(`The sum of all elements in array is : ${sum_of_array(values)}`);
-console.log(`The minimum value in the array is : ${Math.min.apply(Math, values)}`);
-console.log(`The maximum value in the array is :${Math.max.apply(Math, values)}`);
+let sum = 0;
+let minimum = 0;
+let maximum = 0;
+for (let i = 0; i < values.length; i++) {
+  sum += values[i];
+}
+console.log(`The sum of all array values is ${sum}.`)
+for (let i = 0; i < values.length; i++) {
+
+  if (i === 0) {
+    minimum += values[i]
+  }
+  else if (minimum > values[i]) {
+    minimum = values[i];
+  }
+}
+
+
+console.log(`The minimum value of the array is ${minimum}.`)
+
+for (let i = 0; i < values.length; i++) {
+
+  if (i === 0) {
+    maximum += values[i]
+  }
+  else if (maximum < values[i]) {
+    maximum = values[i];
+  }
+}
+
+console.log(`The minimum value of the array is ${maximum}.`)
